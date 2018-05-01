6e0febf7198bed86e18cf68356a78a4bbb50002a
lutfim22 <luthfi.musthafa@gmail.com>
2018-05-01 11:19:11 +0700

    Modul 9 TP

--- app/src/main/java/id/ac/ui/jhotel_android_luthfi/LoginActivity.java
+++ app/src/main/java/id/ac/ui/jhotel_android_luthfi/LoginActivity.java
@@ -2,11 +2,20 @@ package id.ac.ui.jhotel_android_luthfi;
 
 //import android.support.v7.app.AlertDialog;
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
@@ -16,24 +25,43 @@ public class LoginActivity extends AppCompatActivity {
         final EditText emailInput = findViewById(R.id.inputEmail);
         final EditText passInput = findViewById(R.id.inputPass);
         final Button loginButton = findViewById(R.id.buttonLogin);
+        final TextView registerClickable = (TextView) findViewById(R.id.registerClickable);
+
         loginButton.setOnClickListener (new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final String email = emailInput.getText().toString();
                 final String password = passInput.getText().toString();
+                Response.Listener<String> responseListener = new Response.Listener<String> () {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonResponse = new JSONObject(response);
+                            if(jsonResponse!=null) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("Login Success")
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                            builder.setMessage("Login Failed.")
+                                    .create()
+                                    .show();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(email,password,responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+        });
 
-                if(email.equals("test@test.com") && password.equals("test")) {
-                    AlertDialog.Builder builder1 = new AlertDialog.Builder(LoginActivity.this);
-                    builder1.setMessage("Login success.")
-                            .create()
-                            .show();
-                }
-                else {
-                    AlertDialog.Builder builder1 = new AlertDialog.Builder(LoginActivity.this);
-                    builder1.setMessage("Login failed.")
-                            .create()
-                            .show();
-                }
+        registerClickable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regisInt = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(regisInt);
             }
         });
     }
